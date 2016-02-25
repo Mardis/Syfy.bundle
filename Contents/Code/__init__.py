bcb5dfaaefb5c6d3373697a3e198c805354e0c08
Sander <sander@plex.tv>
2016-02-25 17:15:01 +0100

    Update channel and URL Service to support clips.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,5 @@
 SHOWS_URL = 'http://tve-atcnbce.nbcuni.com/live/3/syfy/containers/iPad'
-EPISODES_URL = 'http://tve-atcnbce.nbcuni.com/live/3/syfy/containers/%s/iPad?filterBy=episode'
+VIDEOS_URL = 'http://tve-atcnbce.nbcuni.com/live/3/syfy/containers/%s/iPad'
 
 ####################################################################################################
 def Start():
@@ -21,10 +21,11 @@ def Shows():
 		return oc
 
 	for show in JSON.ObjectFromURL(SHOWS_URL):
+
 		show_id = show['assetID']
 		title = show['title']
 
-		if title in ('Syfy Original Movies'):
+		if 'movies' in title.lower():
 			continue
 
 		summary = show['description']
@@ -37,7 +38,7 @@ def Shows():
 			thumb = Resource.ContentsOfURLWithFallback(thumb)
 		))
 
-	oc.objects.sort(key = lambda obj: obj.title)
+	oc.objects.sort(key=lambda obj: obj.title)
 	return oc
 
 ####################################################################################################
@@ -45,27 +46,64 @@ def Shows():
 def Episodes(show_id, show):
 
 	oc = ObjectContainer(title2=show)
+	clips_available = False
 
-	for episode in JSON.ObjectFromURL(EPISODES_URL % show_id)['results']:
+	for episode in JSON.ObjectFromURL(VIDEOS_URL % show_id)['results']:
+
+		if episode['type'] == 'video' and episode['subtype'] == 'clip' and episode['requiresAuth'] == False:
+			clips_available = True
 
 		if episode['type'] != 'video' or episode['subtype'] != 'episode' or episode['requiresAuth']:
 			continue
 
 		url = 'http://www.syfy.com/#%s|%s' % (show_id, episode['assetID'])
+		thumb = episode['images'][0]['images']['episode_banner'] if 'episode_banner' in episode['images'][0]['images'] else ''
 
 		oc.add(EpisodeObject(
 			url = url,
 			show = show,
 			title = episode['title'],
 			summary = episode['description'],
-			thumb = Resource.ContentsOfURLWithFallback(episode['images'][0]['images']['episode_banner']),
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb),
 			season = int(episode['seasonNumber']),
 			index = int(episode['episodeNumber']),
 			duration = episode['totalDuration'],
 			originally_available_at = Datetime.FromTimestamp(episode['firstAiredDate'])
 		))
 
+	if clips_available:
+
+		oc.add(DirectoryObject(
+			key = Callback(Clips, show_id=show_id, show=show),
+			title = 'Clips'
+		))
+
 	if len(oc) < 1:
-		return ObjectContainer(header='Empty', message='There are no episodes available for this show')
+		return ObjectContainer(header='Empty', message='There are no videos available for this show')
+
+	return oc
+
+####################################################################################################
+@route('/video/syfy/clips/{show_id}')
+def Clips(show_id, show):
+
+	oc = ObjectContainer(title2=show)
+
+	for clip in JSON.ObjectFromURL(VIDEOS_URL % show_id)['results']:
+
+		if clip['type'] != 'video' or clip['subtype'] != 'clip' or clip['requiresAuth']:
+			continue
+
+		url = 'http://www.syfy.com/#%s|%s' % (show_id, clip['assetID'])
+		thumb = clip['images'][0]['images']['episode_banner'] if 'episode_banner' in clip['images'][0]['images'] else ''
+
+		oc.add(VideoClipObject(
+			url = url,
+			title = clip['title'],
+			summary = clip['description'],
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb),
+			duration = clip['totalDuration'],
+			originally_available_at = Datetime.FromTimestamp(clip['firstAiredDate'])
+		))
 
 	return oc
